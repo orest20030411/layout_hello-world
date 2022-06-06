6d76eb1f5acd0de7bb5b34d477b3f7f53ed65043
Orest Yakymiv <akimivorest@gmail.com>
2022-06-06 18:18:42 +0300

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,30 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20220606-181548\\Entire_document.png",
+        "selector": "document",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/hello-world/",
+        "expect": 0,
+        "viewportLabel": "custom-size",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -261,
+            "height": -1025
+          },
+          "misMatchPercentage": "0.33",
+          "analysisTime": 41
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
